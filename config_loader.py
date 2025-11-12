dfce0bc126e6e327414fe2ebb3e4ea5010cc1b83
MG\HalubovichRE <HalubovichRE@cybersell.by>
2025-11-12 17:31:57 +0300

    added an option to dynamically append new api cfg

--- config_loader.py
+++ config_loader.py
@@ -3,7 +3,7 @@ import json
 from models.api_manager import APIManager
 
 def load_configs(stop_event):
-    with open("apis.json", "r", encoding="utf8") as f:
+    with open("apis2.json", "r", encoding="utf8") as f:
         data = json.load(f)
     APIManager.init(data["sources"], stop_event)
     APIManager.start()
